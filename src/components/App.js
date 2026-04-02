c2a48ced60dfe961f627d9343e33f93735cb37fa
mj2141985-spec <mj2141985@gmail.com>
2026-04-02 16:34:36 -0500

    added description, hide vote button if voted

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,120 @@
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './Navigation';
-import Loading from './Loading';
+import Navigation from "./Navigation";
+import Create from "./Create";
+import Proposals from "./Proposals";
+import Loading from "./Loading";
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import DAO_ABI from "../abis/DAO.json";
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from "../config.json";
 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [provider, setProvider] = useState("");
+  const [dao, setDao] = useState(null);
+  const [treasuryBalance, setTreasuryBalance] = useState(0);
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [account, setAccount] = useState(null);
+
+  const [proposals, setProposals] = useState(null);
+  const [quorum, setQuorum] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
     // Initiate provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    // Initiate contracts
+    const dao = new ethers.Contract(
+      config[31337].dao.address,
+      DAO_ABI,
+      provider,
+    );
+    setDao(dao);
+
+    // Fetch treasury balance
+    let treasuryBalance = await provider.getBalance(dao.address);
+    treasuryBalance = ethers.utils.formatUnits(treasuryBalance, 18);
+    setTreasuryBalance(treasuryBalance);
 
     // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    // Fetch proposals count
+    const count = await dao.proposalCount();
+    const items = [];
 
-    setIsLoading(false)
-  }
+    for (let i = 0; i < count.toNumber(); i++) {
+      const proposalId = i + 1;
+      const proposal = await dao.proposals(proposalId);
+      const hasVoted = await dao.votes(account, proposalId);
+
+      items.push({
+        id: proposal.id,
+        name: proposal.name,
+        amount: proposal.amount,
+        recipient: proposal.recipient,
+        votes: proposal.votes,
+        finalized: proposal.finalized,
+        hasVoted,
+      });
+    }
+    setProposals(items);
+
+    // Fetch quorum
+    setQuorum(await dao.quorum());
+
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     if (isLoading) {
-      loadBlockchainData()
+      loadBlockchainData();
     }
   }, [isLoading]);
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className="my-4 text-center">Welcome to our DAO!</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
+          <Create provider={provider} dao={dao} setIsLoading={setIsLoading} />
+
+          <hr />
+
+          <p className="text-center">
+            <strong>Treasury Balance:</strong> {treasuryBalance} ETH
+          </p>
+          
+          <hr />
+
+          <Proposals
+            provider={provider}
+            dao={dao}
+            proposals={proposals}
+            quorum={quorum}
+            setIsLoading={setIsLoading}
+          />
         </>
       )}
     </Container>
-  )
+  );
 }
 
 export default App;
